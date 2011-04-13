11a988c3dbfb8302956dc0b634030634ea1cb346
Alf Simen Sørensen <simen.sorensen@gmail.com>
2011-04-13 17:26:29 +0200

    added Geolocation

--- src/com/alfsimen/bybuss/GoogleMaps.java
+++ src/com/alfsimen/bybuss/GoogleMaps.java
@@ -1,6 +1,11 @@
 package com.alfsimen.bybuss;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 import com.google.android.maps.*;
 
 /**
@@ -15,6 +20,10 @@ public class GoogleMaps extends MapActivity {
     private MapController mapController;
     private GeoPoint lastPoint;
     private MyLocationOverlay myLocOverlay;
+    private ToggleButton geoButton;
+    private Button searchButton;
+    private Button addressButton;
+    private EditText searchBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,13 @@ public class GoogleMaps extends MapActivity {
 
         myLocOverlay = new MyLocationOverlay(this, mapView);
         mapView.getOverlays().add(myLocOverlay);
+
+        geoButton = (ToggleButton) findViewById(R.id.togglebutton_geo);
+        searchButton = (Button) findViewById(R.id.search_button);
+        addressButton = (Button) findViewById(R.id.addressbutton);
+        searchBar = (EditText) findViewById(R.id.search_entry);
+
+        geoButton.setOnClickListener(new GeoButtonClickListener());
     }
 
     public void onPause(Bundle bundle) {
@@ -40,5 +56,32 @@ public class GoogleMaps extends MapActivity {
     protected boolean isRouteDisplayed() {
         //TODO: implement?
         return false;
+    }
+
+    /*
+    * Listeners
+     */
+
+    private final class GeoButtonClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            if(geoButton.isChecked()) {
+                if(!myLocOverlay.enableMyLocation()) {
+                    Toast.makeText(getBaseContext(), "Skru på Wifi/gps-posisjonering i settings", Toast.LENGTH_LONG);
+                    myLocOverlay.disableMyLocation();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Geolokasjon skrudd på", Toast.LENGTH_SHORT).show();
+                    myLocOverlay.runOnFirstFix(new Runnable() {
+                        public void run() {
+                            mapView.getController().animateTo(myLocOverlay.getMyLocation());
+                        }
+                    });
+                }
+            }
+            else {
+                myLocOverlay.disableMyLocation();
+                Toast.makeText(getApplicationContext(), "Geolokasjon skrudd av", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
