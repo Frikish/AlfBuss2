febdde983923804b2bf5b2e5601a0dbf074b2b82
Alf Simen Sørensen <simen.sorensen@gmail.com>
2011-04-14 20:21:35 +0200

    Search implementet with answer over map, find address implementet, reverse search implementet.

--- gen/com/alfsimen/bybuss/R.java
+++ gen/com/alfsimen/bybuss/R.java
@@ -18,13 +18,15 @@ public final class R {
     }
     public static final class id {
         public static final int MapView=0x7f060002;
-        public static final int addressbutton=0x7f060008;
-        public static final int bottomTable=0x7f060006;
+        public static final int addressbutton=0x7f060009;
+        public static final int answer_TV=0x7f060006;
+        public static final int bottomTable=0x7f060007;
         public static final int headerSmallLogoImgVw=0x7f060001;
         public static final int headerTitleTextVw=0x7f060000;
+        public static final int reversebutton=0x7f06000a;
         public static final int search_button=0x7f060005;
         public static final int search_entry=0x7f060004;
-        public static final int togglebutton_geo=0x7f060007;
+        public static final int togglebutton_geo=0x7f060008;
         public static final int top_table=0x7f060003;
     }
     public static final class layout {
@@ -35,11 +37,13 @@ public final class R {
         public static final int holdeplasser=0x7f040000;
     }
     public static final class string {
-        public static final int APIKey=0x7f050006;
-        public static final int APIdebugKey=0x7f050005;
+        public static final int APIKey=0x7f050008;
+        public static final int APIdebugKey=0x7f050007;
         public static final int adress_button=0x7f050004;
+        public static final int answer_field=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int custom_title=0x7f050001;
+        public static final int reverse_button=0x7f050005;
         public static final int search_button=0x7f050002;
         public static final int search_field=0x7f050003;
     }
--- src/com/alfsimen/bybuss/GoogleMaps.java
+++ src/com/alfsimen/bybuss/GoogleMaps.java
@@ -1,18 +1,23 @@
 package com.alfsimen.bybuss;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
 import com.google.android.maps.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,17 +34,25 @@ public class GoogleMaps extends MapActivity {
     private ToggleButton geoButton;
     private Button searchButton;
     private Button addressButton;
+    private Button reverseButton;
+    private TextView answerView;
     private EditText searchBar;
     private XmlParser xmlParser;
     private ArrayList<Holdeplass> holdeplasser;
     private OverlayItem overlayItem;
     private MapsOverlay itemizedOverlay;
     private List<Overlay> mapOverlays;
+    //private List<Overlay> answerOverlay;
+    private AtbBussorakel bussen;
+    private InputMethodManager imm;
+    //private ArrayList<String> addressList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestCustomTitleBar();
         setContentView(R.layout.main);
+        setCustomTitle(getString(R.string.custom_title).toString());
 
         mapView = (MapView) findViewById(R.id.MapView);
         mapView.setBuiltInZoomControls(true);
@@ -59,9 +72,27 @@ public class GoogleMaps extends MapActivity {
         geoButton = (ToggleButton) findViewById(R.id.togglebutton_geo);
         searchButton = (Button) findViewById(R.id.search_button);
         addressButton = (Button) findViewById(R.id.addressbutton);
+        reverseButton = (Button) findViewById(R.id.reversebutton);
         searchBar = (EditText) findViewById(R.id.search_entry);
+        answerView = (TextView) findViewById(R.id.answer_TV);
 
         geoButton.setOnClickListener(new GeoButtonClickListener());
+
+        bussen = new AtbBussorakel();
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        /*
+        *   Searchbaren
+         */
+        searchBar.setMaxWidth(searchBar.getWidth());    //wait wat?
+        searchBar.setOnClickListener(new SearchBarClickListener());
+        searchBar.setOnKeyListener(new SearchBarOnKeyListener());
+        searchBar.setOnFocusChangeListener(new SearchBarOnFoucusChange());
+
+        reverseButton.setOnClickListener(new ReverseButtonOnClickListener());
+
+        addressButton.setOnClickListener(new AddressButtonOnClickListener());
+
+        searchButton.setOnClickListener((new SearchButtonOnClickListener()));
     }
 
     public void onPause(Bundle bundle) {
@@ -76,6 +107,94 @@ public class GoogleMaps extends MapActivity {
     protected boolean isRouteDisplayed() {
         //TODO: implement?
         return false;
+    }
+
+    protected void requestCustomTitleBar()
+    {
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+    }
+
+    protected void setCustomTitle(String msg)
+    {
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_titlebar);
+        TextView tv = (TextView) getWindow().findViewById(R.id.headerTitleTextVw);
+        tv.setText(msg);
+    }
+
+    /*
+    *   div functions
+     */
+
+    private void doSearch() {
+        answerView.setText("Venter på svar fra bussorakelet");
+        if(searchBar.getText().length() <= 0) {
+            answerView.setText("Søkefeltet er tomt -_-");
+        }
+        else {
+            imm.hideSoftInputFromWindow(searchBar.getWindowToken(), 0);
+            bussen.setQuestion(searchBar.getText().toString().trim());
+
+            new AtbThreadTest(this).execute();
+        }
+    }
+
+    private void reverseSearch()
+    {
+        String text = searchBar.getText().toString();
+        String words [] = text.split(" ");
+        boolean til = false;
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].equalsIgnoreCase("til")) {
+                til = true;
+            }
+        }
+        if(searchBar.getText().length() <= 0)
+        {
+            answerView.setText("Søkefeltet er tomt -.-");
+        }
+        else if(til && words.length < 3) {
+            answerView.setText("Trenger 2 holdeplasser og ordet 'til' imellom de");
+        }
+        else if(searchBar.getText().toString().equals(getString(R.string.search_field)))
+        {
+            answerView.setText("Du må skrive inn noen holdeplasser, nå er det bare dummytekst i feltet");
+        }
+        else if(words.length >= 3 && til)
+        {
+            String tmp = searchBar.getText().toString();
+            String [] parts = tmp.split("til");
+            if(parts.length == 2)
+            {
+                parts = tmp.split(" til ");
+                String newString = parts[1] + " til " + parts[0];
+                searchBar.setText(newString);
+
+                doSearch();
+            }
+            else
+            {
+                answerView.setText("Trenger 2 holdeplasser, prøv igjen...");
+            }
+        }
+    }
+
+    public void getAddressesOfCurrentPos(GeoPoint point) {
+        Geocoder geoCoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try{
+            List<Address> addresses = geoCoder.getFromLocation(point.getLatitudeE6() / 1E6, point.getLongitudeE6() / 1E6, 1);
+
+            String add = "";
+            if(addresses.size() > 0) {
+                for(int i = 0; i < addresses.get(0).getMaxAddressLineIndex() -1; i++) {
+                    add += addresses.get(0).getAddressLine(i);
+                }
+            }
+            searchBar.setText(add + " til ");
+            searchBar.setSelection(searchBar.getText().toString().length());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /*
@@ -102,6 +221,50 @@ public class GoogleMaps extends MapActivity {
                 myLocOverlay.disableMyLocation();
                 Toast.makeText(getApplicationContext(), "Geolokasjon skrudd av", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    private final class SearchBarClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            if(searchBar.getText().toString().equals(getString(R.string.search_field))) {
+                searchBar.setText("");
+            }
+        }
+    }
+
+    private final class SearchBarOnKeyListener implements View.OnKeyListener {
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                doSearch();
+            }
+            return false;
+        }
+    }
+
+    private final class SearchBarOnFoucusChange implements View.OnFocusChangeListener {
+        public void onFocusChange(View v, boolean hasFocus) {
+            if(!hasFocus) {
+                imm.hideSoftInputFromWindow(searchBar.getWindowToken(), 0);
+            }
+        }
+    }
+
+    private final class ReverseButtonOnClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            reverseSearch();
+        }
+    }
+
+    private final class AddressButtonOnClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            if(myLocOverlay.getMyLocation() != null)
+                getAddressesOfCurrentPos(myLocOverlay.getMyLocation());
+        }
+    }
+
+    private final class SearchButtonOnClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            doSearch();
         }
     }
 
@@ -141,6 +304,69 @@ public class GoogleMaps extends MapActivity {
                 mapController.setCenter(new GeoPoint((int) (63.4181 * 1E6), (int) (10.4057 * 1E6)));
             }
             Toast.makeText(getApplicationContext(), "Loading av bussholdeplasser ferdig", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+/*    class getAddressOfCurrentPos extends AsyncTask<GeoPoint, Void, Void> {
+        @Override
+        protected Void doInBackground(GeoPoint... points) {
+            int count = points.length;
+            for(int i = 0; i < count; i++) {
+                try {
+                    Geocoder geoCoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = geoCoder.getFromLocation(points[i].getLatitudeE6() / 1E6, points[i].getLongitudeE6() /1E6, 1);
+                    String add = "";
+                    if(addresses.size() > 0) {
+                        for(int j = 0; j < addresses.get(0).getMaxAddressLineIndex(); j++) {
+                            add += addresses.get(0).getAddressLine(j);
+                        }
+                    }
+                    searchBar.setText(add);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }  */
+
+    /**
+     * Threading for search query
+     *
+     * @author tmn
+     */
+    class AtbThreadTest extends AsyncTask<Void, Void, Void>
+    {
+        private Context context;
+
+        public AtbThreadTest(Context context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            bussen.ask();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused)
+        {
+            if (bussen.getAnswer().trim().equals("No question supplied."))
+            {
+                answerView.setText(getString(R.string.answer_field));
+            } else
+            {
+                //if (!isInDatabase(context, searchBar.getText().toString().trim()))
+                //{
+                //    new ListUpdateThread(1, -1).execute();
+                //}
+                answerView.setText(bussen.getAnswer());
+               // new MarkBusStops(getApplicationContext()).execute();
+            }
         }
     }
 }
