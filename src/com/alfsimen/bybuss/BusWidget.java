54e7554b9fc50ef8493293a252e911afbb052862
Alf Simen Sørensen <simen.sorensen@gmail.com>
2011-05-03 20:33:18 +0200

    The scaffolding for a widget has been added

---
+++ src/com/alfsimen/bybuss/BusWidget.java
@@ -0,0 +1,11 @@
+package com.alfsimen.bybuss;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alf
+ * Date: 5/3/11
+ * Time: 8:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BusWidget {
+}
---
+++ src/com/alfsimen/bybuss/BusWidgetProvider.java
@@ -0,0 +1,53 @@
+package com.alfsimen.bybuss;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alf
+ * Date: 5/3/11
+ * Time: 7:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BusWidgetProvider extends AppWidgetProvider{
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for(int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, BusWidget.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.bus_widget_ui);
+            remoteView.setOnClickPendingIntent(R.id.widgetSearchButton, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteView);
+        }
+    }
+}
